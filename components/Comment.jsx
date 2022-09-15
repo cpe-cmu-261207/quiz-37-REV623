54150f596538ad844bb087f035681130f5fe71eb
Reverse <jedsadakorn_k@cmu.ac.th>
2022-09-15 14:21:25 +0700

    finished

--- components/Comment.jsx
+++ components/Comment.jsx
@@ -5,9 +5,39 @@ export default function Comment(props) {
   return (
     <div>
       {/* Entire Comment div */}
-      <div></div>
+      <div className="d-flex gap-2 my-2">
+          <img
+            src={props.img}
+            width="48"
+            height="48"
+            className="rounded-circle"
+            style={{ objectFit: "cover" }}
+          />
+          <div
+            className="rounded rounded-3 p-2"
+            style={{ backgroundColor: "#E5E7EB" }}
+          >
+            <span className="fw-semibold">{props.user}</span>
+            <br />
+            <span>{props.text}</span>
+            { 
+              props.like > 0 ? 
+              <div className="d-flex align-items-center gap-1">
+                <img src="/like.svg" width={20}></img>
+                <span className="text-muted">{props.like} คน</span>
+              </div>
+              : <div></div>
+            }
+          </div>
+        </div>
 
       {/*render Reply here... */}
+      {props.replies.map((x) => (<Reply 
+        user={x.username} 
+        img={x.userImagePath} 
+        text={x.replyText} 
+        like={x.likeNum} 
+        />))}
     </div>
   );
 }
--- components/PostOwner.jsx
+++ components/PostOwner.jsx
@@ -1,5 +1,34 @@
 import React from "react";
+import Comment from "./Comment";
 
 export default function PostOwner(props) {
-  return <div></div>;
+  return (
+    <div>
+      <div className="vstack gap-3">
+        <div className="d-flex align-items-center gap-3">
+          <img
+            src="/profileImages/popcat.png"
+            width="48"
+            height="48"
+            className="rounded-circle"
+            style={{ objectFit: "cover" }}
+          />
+          <span className="fw-semibold fs-5">Jedsadakorn Kritsadakul 640610623</span>
+        </div>
+        <span>Quiz ง่ายจังเลยครับ ขอยาก ๆ กว่านี้ได้ไหม #261207</span>
+        <div className="d-flex align-items-center gap-1">
+          <img src="/like.svg" width={20}></img>
+          <span className="text-muted">111 คน</span>
+        </div>
+        <hr className="m-0" />
+      </div>
+      {props.comments.map((x) => (<Comment 
+        user={x.username} 
+        img={x.userImagePath} 
+        text={x.commentText} 
+        like={x.likeNum} 
+        replies={x.replies} 
+        />))}
+    </div>
+  )
 }
